3255b08511967a9cc21950bcd8f699cbaf4bf00c
sanao1006 <naosak1006@gmail.com>
2023-03-15 14:36:15 +0900

    modifying MainActivity.kt  - companion object

--- app/src/main/java/com/example/otenki/MainActivity.kt
+++ app/src/main/java/com/example/otenki/MainActivity.kt
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private const val DEBUG_TAG = "AsyncSample"
+        private const val WEATHER_INFO_URL = "https://api.openweathermap.org/data/2.5/weather?lang=ja"
+        private const val APP_ID = "1a5c1da9c26466390fd4f592fa517ff9"
+    }
+    private var _list: MutableList<MutableMap<String, String>> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
